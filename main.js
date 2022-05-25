bd42b4c875b0951697fde904beaddd02f57ba23f
gostaar <99218705+gostaar@users.noreply.github.com>
2022-05-25 06:30:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- * ---
- * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
- **/
-
 var inputs = readline().split(' ');
 const lightX = parseInt(inputs[0]); // the X position of the light of power
 const lightY = parseInt(inputs[1]); // the Y position of the light of power
-const initialTx = parseInt(inputs[2]); // Thor's starting X position
-const initialTy = parseInt(inputs[3]); // Thor's starting Y position
+const Tx = parseInt(inputs[2]); // Thor's starting X position
+const Ty = parseInt(inputs[3]); // Thor's starting Y position
+let ThorX = Tx;
+let ThorY = Ty;
 
 // game loop
 while (true) {
     const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
+   let directionX = '';
+   let directionY = '';
 
-    // Write an action using console.log()
-    // To debug: console.error('Debug messages...');
+   if (ThorX > lightX){
+    directionX = "W";
+    ThorX--;
+   } else if (ThorX < lightX){
+    directionX = "E";
+    ThorX++;
+   } else { directionX = ""; }
+
+   if (ThorY > lightY){
+       directionY = "N";
+       ThorY--;
+   } else if (ThorY < lightY) {
+       directionY = "S";
+       ThorY++;
+   } else { directionY = ""; }
+
+   print(directionY + directionX);
+     
+}
